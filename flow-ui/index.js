ab651a2e26b5dde5d10781910c4f557c2120573e
Ryan Shaw <ryanshaw@unc.edu>
2016-02-25 21:44:32 -0500

    Move flow UI into its own dir.

--- flow-ui/index.js
+++ flow-ui/index.js
@@ -2,8 +2,6 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-//import BasicUI from './basic-ui.jsx'
-//import SegmentsUI from './segments-ui.jsx'
 import FlowUI from './flow-ui.jsx'
 import 'whatwg-fetch'
 
